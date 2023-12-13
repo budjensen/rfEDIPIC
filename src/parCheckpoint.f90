49d06032f649317e3e92853e45e08e128431d54f
budjensen <bjensen@princeton.edu>
2023-12-13 10:12:11 -0500

    Modified syntax for increased readability

--- src/parCheckpoint.f90
+++ src/parCheckpoint.f90
@@ -1,84 +1,84 @@
-SUBROUTINE SAVE_CHECKPOINT
+subroutine SAVE_CHECKPOINT
 
-   USE CurrentProblemValues
-   USE ParallelOperationValues
-   USE Diagnostics
-   USE Snapshots
-   USE mt19937
+   use CurrentProblemValues
+   use ParallelOperationValues
+   use Diagnostics
+   use Snapshots
+   use mt19937
 
-   IMPLICIT NONE
+   implicit none
 
-   INTEGER n ! number of particle
+   integer n ! number of particle
 
-   REAL R
+   real R
 
-   IF (T_cntr.NE.Save_check_Tcntr) RETURN
+   if (T_cntr.NE.Save_check_Tcntr) return
 
-   IF (Rank_of_process.EQ.0) THEN
+   if (Rank_of_process.EQ.0) then
 
-      OPEN (9, FILE = check_g_filename, STATUS = 'REPLACE')
+      open (9, file = check_g_filename, status = 'replace')
 
       I_random_seed = int(2.d0**31 * grnd())
       if (I_random_seed.le.0) I_random_seed = (2**31 - 1) + I_random_seed
 
-      WRITE (9, '(2x,i14,2x,i14)') T_cntr,           I_random_seed
-      WRITE (9, '(2x,i14,2x,i14)') Start_diag_Tcntr, current_snap
-      WRITE (9, '(2x,i14,2x,i14)') N_part(1),        N_part(2)
-      WRITE (9, '(2x,i14,2x,i14)') Q_left,           Q_right
-      WRITE (9, '(2x,e14.7,2x,e14.7,2x,e14.7)') full_Q_left,  full_Q_right , Q_ext
+      write (9, '(2x,i14,2x,i14)') T_cntr,           I_random_seed
+      write (9, '(2x,i14,2x,i14)') Start_diag_Tcntr, current_snap
+      write (9, '(2x,i14,2x,i14)') N_part(1),        N_part(2)
+      write (9, '(2x,i14,2x,i14)') Q_left,           Q_right
+      write (9, '(2x,e14.7,2x,e14.7,2x,e14.7)') full_Q_left,  full_Q_right , Q_ext
 
-      WRITE (9, '(2x,i14,2x,i14)')     N_of_saved_records, text_output_counter
-      WRITE (9, '(2x,e14.7,2x,e14.7)') Energy_full_eV,     Init_energy_full_eV
-      WRITE (9, '(2x,e14.7,2x,e14.7)') Energy_pot_eV(1),   Energy_pot_eV(2)
-      WRITE (9, '(2x,e14.7,2x,e14.7)') Energy_heat_eV(1),  Energy_heat_eV(2)
-      WRITE (9, '(2x,e14.7,2x,e14.7)') Energy_kin_eV(1),   Energy_kin_eV(2)
-      WRITE (9, '(2x,e14.7,2x,e14.7)') Energy_wall_eV(1),  Energy_wall_eV(2)
-      WRITE (9, '(2x,e14.7,2x,e14.7)') Energy_emit_eV(1),  Energy_emit_eV(2)
-      WRITE (9, '(2x,e14.7,2x,e14.7)') Energy_coll_eV(1),  Energy_coll_eV(2)
-      WRITE (9, '(2x,e14.7,2x,e14.7)') prev_Q_left,      prev_Q_right
-      CLOSE (9, STATUS = 'KEEP')
+      write (9, '(2x,i14,2x,i14)')     N_of_saved_records, text_output_counter
+      write (9, '(2x,e14.7,2x,e14.7)') Energy_full_eV,     Init_energy_full_eV
+      write (9, '(2x,e14.7,2x,e14.7)') Energy_pot_eV(1),   Energy_pot_eV(2)
+      write (9, '(2x,e14.7,2x,e14.7)') Energy_heat_eV(1),  Energy_heat_eV(2)
+      write (9, '(2x,e14.7,2x,e14.7)') Energy_kin_eV(1),   Energy_kin_eV(2)
+      write (9, '(2x,e14.7,2x,e14.7)') Energy_wall_eV(1),  Energy_wall_eV(2)
+      write (9, '(2x,e14.7,2x,e14.7)') Energy_emit_eV(1),  Energy_emit_eV(2)
+      write (9, '(2x,e14.7,2x,e14.7)') Energy_coll_eV(1),  Energy_coll_eV(2)
+      write (9, '(2x,e14.7,2x,e14.7)') prev_Q_left,      prev_Q_right
+      close (9, status = 'keep')
 
    ELSE
 
-      OPEN (9, FILE = check_g_filename, STATUS = 'REPLACE')
+      open (9, file = check_g_filename, status = 'replace')
 
       I_random_seed = int(2.d0**31 * grnd())
       if (I_random_seed.le.0) I_random_seed = (2**31 - 1) + I_random_seed
 
-      WRITE (9, '(2x,i14,2x,i14)') T_cntr,           I_random_seed
-      WRITE (9, '(2x,i14,2x,i14)') Start_diag_Tcntr, current_snap
-      WRITE (9, '(2x,i14,2x,i14)') N_part(1),        N_part(2)
-      WRITE (9, '(2x,i14,2x,i14)') Q_left,           Q_right
-      CLOSE (9, STATUS = 'KEEP')
+      write (9, '(2x,i14,2x,i14)') T_cntr,           I_random_seed
+      write (9, '(2x,i14,2x,i14)') Start_diag_Tcntr, current_snap
+      write (9, '(2x,i14,2x,i14)') N_part(1),        N_part(2)
+      write (9, '(2x,i14,2x,i14)') Q_left,           Q_right
+      close (9, status = 'keep')
 
-      OPEN (9, FILE = check_e_filename, STATUS = 'REPLACE')
-      DO n = 1, N_part(1)
-         WRITE (9, '(5(1x,e16.9),1x,i4)')  X_of_spec(1)%part(n), &
+      open (9, file = check_e_filename, status = 'replace')
+      do n = 1, N_part(1)
+         write (9, '(5(1x,e16.9),1x,i4)')  X_of_spec(1)%part(n), &
          & VX_of_spec(1)%part(n), &
          & VY_of_spec(1)%part(n), &
          & VZ_of_spec(1)%part(n), &
          & AX_of_spec(1)%part(n), &
          & Tag_of_spec(1)%part(n)
-      END DO
-      CLOSE (9, STATUS = 'KEEP')
+      end do
+      close (9, status = 'keep')
 
-      IF (N_spec.EQ.2) THEN
-         OPEN (9, FILE = check_i_filename, STATUS = 'REPLACE')
-         DO n = 1, N_part(2)
-            WRITE (9, '(5(1x,e16.9),1x,i4)')  X_of_spec(2)%part(n), &
+      if (N_spec.EQ.2) then
+         open (9, file = check_i_filename, status = 'replace')
+         do n = 1, N_part(2)
+            write (9, '(5(1x,e16.9),1x,i4)')  X_of_spec(2)%part(n), &
             & VX_of_spec(2)%part(n), &
             & VY_of_spec(2)%part(n), &
             & VZ_of_spec(2)%part(n), &
             & AX_of_spec(2)%part(n), &
             & Tag_of_spec(2)%part(n)
-         END DO
-         CLOSE (9, STATUS = 'KEEP')
-      END IF
+         end do
+         close (9, status = 'keep')
+      end if
 
-   END IF
+   end if
 
-   PRINT '(2x,"Process ",i3," : created the new checkpoint...")', Rank_of_process
+   print '(2x,"Process ",i3," : created the new checkpoint...")', Rank_of_process
 
    Save_check_Tcntr = Save_check_Tcntr + SaveCheck_step
 
-END SUBROUTINE SAVE_CHECKPOINT
+end subroutine SAVE_CHECKPOINT
--- src/parMainProgram.f90
+++ src/parMainProgram.f90
@@ -1,127 +1,127 @@
 !===========================================
-PROGRAM MainProg
+program MainProg
 
    use mpi
-   USE CurrentProblemValues
-   USE ParallelOperationValues
+   use CurrentProblemValues
+   use ParallelOperationValues
 
-   IMPLICIT NONE
+   implicit none
 
-!  INCLUDE 'mpif.h'
+!  include 'mpif.h'
 
-   INTEGER ierr
-   REAL(8) start, finish
+   integer ierr
+   real(8) start, finish
 
-   CALL MPI_INIT(ierr)
-   CALL MPI_COMM_RANK(MPI_COMM_WORLD, Rank_of_process, ierr)
-   CALL MPI_COMM_SIZE(MPI_COMM_WORLD, N_of_processes, ierr)
+   call MPI_INIT(ierr)
+   call MPI_COMM_RANK(MPI_COMM_WORLD, Rank_of_process, ierr)
+   call MPI_COMM_SIZE(MPI_COMM_WORLD, N_of_processes, ierr)
 
 !  print *, 'my rank is ', Rank_of_process
 
-   CALL INITIATE_PARAMETERS         ! all, with some differences, distribute particles over nodes here
+   call INITIATE_PARAMETERS         ! all, with some differences, distribute particles over nodes here
 
-   CALL INITIATE_MC_COLLISIONS
-   CALL INITIATE_SE_EMISSION        ! all, with some differences
-   CALL INITIATE_ELECTRON_INJECTION
-   CALL INITIATE_BEAM_IN_PLASMA
-   CALL INITIATE_COULOMB_COLLISIONS
+   call INITIATE_MC_COLLISIONS
+   call INITIATE_SE_EMISSION        ! all, with some differences
+   call INITIATE_ELECTRON_INJECTION
+   call INITIATE_BEAM_IN_PLASMA
+   call INITIATE_COULOMB_COLLISIONS
 
-   CALL INITIATE_DIAGNOSTICS
+   call INITIATE_DIAGNOSTICS
 
-   IF (Rank_of_process.EQ.0) THEN
-      CALL PREPARE_TIME_DEPENDENCE_DATAFILES         ! server only
-!restore this later     CALL INITIATE_TEST_PARTICLES                   ! server only
-   END IF
+   if (Rank_of_process.EQ.0) then
+      call PREPARE_TIME_DEPENDENCE_DATAFILES         ! server only
+!restore this later     call INITIATE_TEST_PARTICLES                   ! server only
+   end if
 
    start = MPI_WTIME()
 
-   DO T_cntr = Start_T_cntr, Max_T_cntr
+   do T_cntr = Start_T_cntr, Max_T_cntr
 
       if (Rank_of_process.eq.0) start = MPI_WTIME()
 
 !!! print *, T_cntr, Rank_of_process, N_part(1), N_part(2)
 
-      IF (T_cntr.NE.Start_T_cntr) THEN
-         CALL CALCULATE_STR_CHARGE_DENSITY                                   ! server and clients, with difference
-         CALL CALCULATE_STR_LONG_ELECTR_FIELD                                ! server and clients, with difference
+      if (T_cntr.NE.Start_T_cntr) then
+         call CALCULATE_STR_CHARGE_DENSITY                                   ! server and clients, with difference
+         call CALCULATE_STR_LONG_ELECTR_FIELD                                ! server and clients, with difference
 
-         IF (Rank_of_process.GT.0) THEN
-            CALL FINAL_PUSH                                                  ! clients
-            CALL INJECT_ADDITIONAL_EI_PAIRS_LEFT_WALL                        ! clients
-            CALL INJECT_ELECTRONS_AT_WALLS                                   ! clients
-         ELSE
-!restore this later           CALL TEST_PARTICLES_COMBINED_PUSH_AND_SAVE                       ! server
-         END IF
+         if (Rank_of_process.GT.0) then
+            call FINAL_PUSH                                                  ! clients
+            call INJECT_ADDITIONAL_EI_PAIRS_LEFT_WALL                        ! clients
+            call INJECT_ELECTRONS_AT_WALLS                                   ! clients
+         else
+!restore this later           call TEST_PARTICLES_COMBINED_PUSH_AND_SAVE                       ! server
+         end if
 
-         CALL MANAGE_WALL_CHARGE_DENSITIES                                   ! server and clients, with difference
+         call MANAGE_WALL_CHARGE_DENSITIES                                   ! server and clients, with difference
 
-         CALL PROCESS_COLLISIONS_WITH_ATOMS                                  ! server and clients, with difference
+         call PROCESS_COLLISIONS_WITH_ATOMS                                  ! server and clients, with difference
 
-         CALL PROCESS_COLLISIONS_EL_EL_LNGV                                  ! server and clients, with difference
+         call PROCESS_COLLISIONS_EL_EL_LNGV                                  ! server and clients, with difference
 
-         CALL START_BEAM_IN_PLASMA                                           ! server and clients, with difference, only one time
+         call START_BEAM_IN_PLASMA                                           ! server and clients, with difference, only one time
 
-         IF (Rank_of_process.GT.0) CALL REARRANGE_ACTIVE_BLOCKS              ! clients
+         if (Rank_of_process.GT.0) call REARRANGE_ACTIVE_BLOCKS              ! clients
 
-!        CALL CLEAR_TAGS                                 ! clients
+!        call CLEAR_TAGS                                 ! clients
 
-         IF (Rank_of_process.GT.0) CALL CLEAR_ELECTRON_LIST                  ! clients
-         IF (Rank_of_process.GT.0) CALL CLEAR_ION_LIST                       ! clients
+         if (Rank_of_process.GT.0) call CLEAR_ELECTRON_LIST                  ! clients
+         if (Rank_of_process.GT.0) call CLEAR_ION_LIST                       ! clients
 
-         CALL MPI_BARRIER(MPI_COMM_WORLD, ierr)                              ! all, synchronization   !!!! comment me !!!!
+         call MPI_BARRIER(MPI_COMM_WORLD, ierr)                              ! all, synchronization   !!!! comment me !!!!
 
-      END IF
+      end if
 
       ! Save diagnostics data, if inside of a diagnostics window [Start_diag_Tcntr, Finish_diag_Tcntr]
-      CALL DO_DIAGNOSTICS_STEP_1                                             ! server and clients, with differences
+      call do_DIAGNOSTICS_STEP_1                                             ! server and clients, with differences
 
-      CALL CREATE_SNAPSHOT                                                   ! server and clients, with differences
+      call CREATE_SNAPSHOT                                                   ! server and clients, with differences
 
       ! If we reach Finish_diag_Tcntr, clear diagnostics arrays and set new a new diagnostics window
-      CALL DO_DIAGNOSTICS_STEP_2                                             ! server and clients, with differences
+      call do_DIAGNOSTICS_STEP_2                                             ! server and clients, with differences
 
-      IF (SaveCheck_step.GE.0) CALL SAVE_CHECKPOINT                          ! server and clients, with differences
+      if (SaveCheck_step.GE.0) call SAVE_CHECKPOINT                          ! server and clients, with differences
 
-      CALL MPI_BARRIER(MPI_COMM_WORLD, ierr)                                 ! all, synchronization
+      call MPI_BARRIER(MPI_COMM_WORLD, ierr)                                 ! all, synchronization
 
-      IF (Rank_of_process.GT.0) THEN
-         CALL PREALLOCATE_ELECTRON_LIST                                      ! clients
-         CALL PREALLOCATE_ION_LIST                                           ! clients
-         CALL PREDICTING_PUSH                                                ! clients
-      ELSE
-!restore this later        CALL ACTIVATE_TEST_PARTICLE_RUN                                     ! server
-      END IF
+      if (Rank_of_process.GT.0) then
+         call PREALLOCATE_ELECTRON_LIST                                      ! clients
+         call PREALLOCATE_ION_LIST                                           ! clients
+         call PREDICTING_PUSH                                                ! clients
+      else
+!restore this later        call ACTIVATE_TEST_PARTICLE_RUN                                     ! server
+      end if
 
-      CALL MPI_BARRIER(MPI_COMM_WORLD, ierr)                                 ! all, synchronization
+      call MPI_BARRIER(MPI_COMM_WORLD, ierr)                                 ! all, synchronization
 
 !if (Rank_of_process.eq.0) then
 !   finish = MPI_WTIME()
-!   if (mod(T_cntr,17).eq.0) PRINT '(2x,"**** cycle ",i8," time is  : ", f11.6," sec")', T_cntr, finish - start
+!   if (mod(T_cntr,17).eq.0) print '(2x,"**** cycle ",i8," time is  : ", f11.6," sec")', T_cntr, finish - start
 !end if
 
-   END DO
+   end do
 
    finish = MPI_WTIME()
 
-   PRINT '(2x,"**** Process ",i3" : Simulation time is  : ", f11.3," sec")', Rank_of_process, finish - start
+   print '(2x,"**** Process ",i3" : Simulation time is  : ", f11.3," sec")', Rank_of_process, finish - start
 
-   IF (Rank_of_process.GT.0) CALL CLEAR_ELECTRON_LIST                        ! clients
-   IF (Rank_of_process.GT.0) CALL CLEAR_ION_LIST                             ! clients
+   if (Rank_of_process.GT.0) call CLEAR_ELECTRON_LIST                        ! clients
+   if (Rank_of_process.GT.0) call CLEAR_ION_LIST                             ! clients
 
-   CALL REMOVE_MESH_ARRAYS                               ! all
-   IF (Rank_of_process.GT.0) CALL REMOVE_PARTICLE_DYNAM_ARRAYS               ! clients (server calls it in INITIATE_PARAMETERS : INITIATE_PARTICLE_DYNAM_ARRAYS)
+   call REMOVE_MESH_ARRAYS                               ! all
+   if (Rank_of_process.GT.0) call REMOVE_PARTICLE_DYNAM_ARRAYS               ! clients (server calls it in INITIATE_PARAMETERS : INITIATE_PARTICLE_DYNAM_ARRAYS)
 
-   CALL REMOVE_CRSECT_ARRAYS
-   CALL REMOVE_COLLSPEC_ARRAYS
-   CALL REMOVE_COLLISION_ARRAYS
-   CALL REMOVE_LANGEVIN_ARRAYS
+   call REMOVE_CRSECT_ARRAYS
+   call REMOVE_COLLSPEC_ARRAYS
+   call REMOVE_COLLISION_ARRAYS
+   call REMOVE_LANGEVIN_ARRAYS
 
-   CALL FINISH_SNAPSHOTS
-   CALL FINISH_DIAGNOSTICS
-!restore this later  IF (Rank_of_process.EQ.0) CALL REMOVE_TEST_PARTICLE_ARRAYS                ! server only
+   call FINISH_SNAPSHOTS
+   call FINISH_DIAGNOSTICS
+!restore this later  if (Rank_of_process.EQ.0) call REMOVE_TEST_PARTICLE_ARRAYS                ! server only
 
    call MPI_FINALIZE(ierr)
 
-END PROGRAM MainProg
+end program MainProg
 
 
